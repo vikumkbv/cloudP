e98f034be4ec5c08d7d2ffb7af7315e8022472bc
Gigara <info@gigara.info>
2020-10-02 11:40:14 +0530

    Backend Initialization

---
+++ backend/store/src/main/java/com/giga/store/FashionStoreApplication.java
@@ -0,0 +1,13 @@
+package com.giga.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionStoreApplication.class, args);
+	}
+
+}
---
+++ backend/store/src/test/java/com/giga/store/FashionStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giga.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
