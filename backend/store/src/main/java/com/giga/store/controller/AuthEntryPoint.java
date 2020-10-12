dedf9b61c6ed5b39c3bf86a283c38731a2304399
Charith Jayasanka <charithjayasanka2@gmail.com>
2020-10-12 17:55:00 +0530

    add auth entrypoint

---
+++ backend/store/src/main/java/com/giga/store/controller/AuthEntryPoint.java
@@ -0,0 +1,22 @@
+package com.giga.FashionStore.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Unauthorized exceptions handler.
+ */
+@Component
+public class AuthEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Invalid Credentials");
+    }
+}
